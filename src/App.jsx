42c5f4cc21bf58334a81d3a91b26a5bb6ad3b1d9
JoselinKato <kato2601@outlook.com>
2025-11-10 00:44:10 -0600

    actualización de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { Routes, Route, Link } from 'react-router-dom';
+// src/App.jsx
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import Login from './pages/Login';
+import PanelAdministrativo from './pages/panelAdministrativo';
+
+import Navbar from './components/Navbar'; 
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const routeTitles = {
+    '/': 'Bienvenido',
+    '/administrativo': 'Panel Administrativo'
+  };
+
+  const currentTitle = routeTitles[location.pathname] || 'Página';
+  const handleBack = () => navigate(-1); 
+
+  const showNavbar = location.pathname !== '/';
+
   return (
     <div>
-      <div className="">
+      {showNavbar && <Navbar title={currentTitle} onBack={handleBack} />}
+
+      <main>
         <Routes>
           <Route path="/" element={<Login />} />
+
+          <Route path="/administrativo" element={<PanelAdministrativo />} />
+        
         </Routes>
-      </div>
+      </main>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,31 @@
 import FormularioLogin from "../components/Formulario-login";
 import "./../styles/Login.css";
+import { Row } from "react-bootstrap";
+import Logo from "../../public/favicon.ico";
 
 function Home() {
   return (
     <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 col-md vh-100 bg-white text-white text-center justify-content-center align-item-center">
+      <Row>
+        <div className="divpadre col-12 col-md vh-100 text-center justify-content-center align-item-center">
           <div className="d-flex justify-content-start color-principal-letra p-3">
-            <h5 className="m-0 fw-bold">SaludxChiapas</h5>
+            {/* <h5 className="m-0 fw-bold">SaludxChiapas</h5> */}
           </div>
-          <h4 className="color-principal-letra">¡Bienvenido!</h4>
-          <FormularioLogin />
+          <h4 className="color-principal-letra mt-5">¡Bienvenido!</h4>
+          <div>
+            <FormularioLogin />
+          </div>
+          
         </div>
-        <div className="col-12 col-md vh-100 fondo-login-derecha text-white text-center"></div>
-      </div>
+        <div className="col-12 col-md vh-100 fondo-login-derecha text-white text-center justify-content-center align-item-center d-flex flex-column">
+          <img 
+         src={Logo} 
+         alt="Logo" 
+         className="img-fluid" 
+         style={{marginLeft:"25%", maxWidth: '50%', height: 'auto' }} 
+        />
+        </div>
+      </Row>
     </div>
   );
 }
